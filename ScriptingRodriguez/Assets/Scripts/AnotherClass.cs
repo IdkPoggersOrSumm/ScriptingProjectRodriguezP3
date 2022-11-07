f1d32db0d12cb033b28bc8320e2c8bc476773966
IdkPoggersOrSumm <Somethingtodowithsharks@gmail.com>
2022-11-07 10:51:40 -0800

    Added New Scripting Project
    Added Two new scripts

---
+++ ScriptingRodriguez/Assets/Scripts/AnotherClass.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AnotherClass : MonoBehaviour
+{
+    public int apples;
+    public int bannanas;
+
+    private int stapler;
+    private int stellotape;
+
+    public void FruitMachine(int a, int b)
+    {
+        int answer;
+        answer = a + b;
+        Debug.Log("Fruit total: " + answer);
+    }
+
+    private void OfficeSort(int a, int b)
+    {
+        int answer;
+        answer = a + b;
+        Debug.Log("Office Supplies total: " + answer);
+    }
+}
