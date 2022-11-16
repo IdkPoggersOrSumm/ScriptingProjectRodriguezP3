231e157dee42763a9faa8189eede7d891ef400e5
IdkPoggersOrSumm <Somethingtodowithsharks@gmail.com>
2022-11-16 11:42:06 -0800

    Added New Scripting Project
    Added Awake and Start Scripting Project

---
+++ ScriptingRodriguez/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+    void Start()
+    {
+        Debug.Log("Start Called.");
+    }
+}
